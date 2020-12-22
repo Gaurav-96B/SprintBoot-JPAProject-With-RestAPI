c524aef82215dbfc8ee8afcacde9d94be9bb092f
ADMIN <ADMIN@192.168.2.13>
2020-12-22 21:44:01 +0530

    First commit

--- SpringBoot_JpaProject_With_RestAPI/src/main/java/com/SpringBootJPAProject/SpringBootJPAProject/DAO/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBootJPAProject.SpringBootJPAProject.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.SpringBootJPAProject.SpringBootJPAProject.Model.Book;
-
-public interface BookRepository extends CrudRepository<Book,Integer> {
-
-}
